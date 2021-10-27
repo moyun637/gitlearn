c7858a31be535b0527cef0a8b38582cb6eb8da00
nima <1285583437@qq.com>
2021-10-27 20:59:57 +0800

    更新2

--- src/main/java/com/youibot/learn/demo01/Hello.java
+++ src/main/java/com/youibot/learn/demo01/Hello.java
@@ -3,5 +3,7 @@ package com.youibot.learn.demo01;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("git");
+        System.out.println("更新2");
     }
 }
